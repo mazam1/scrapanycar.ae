25fef1ebbc0b0f7eb9967fff7bd5468a6fa20fb8
Azam <azamdaud2@gmail.com>
2025-11-20 19:16:12 +0500

    more fixes and blogs removal

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -14,7 +14,6 @@ const navigation = [
   { name: "Home", href: "/" },
   { name: "About Us", href: "/about" },
   { name: "Services", href: "/services" },
-  { name: "Blog", href: "/blog" },
   { name: "Contact", href: "/contact" },
 ]
 
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -15,7 +15,6 @@ const navigation = [
   { name: "Home", href: "/" },
   { name: "About Us", href: "/about" },
   { name: "Services", href: "/services" },
-  { name: "Blog", href: "/blog" },
   { name: "Contact", href: "/contact" },
 ]
 
