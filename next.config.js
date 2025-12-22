9180c5517d5eef880efe39870d1ef56b07166484
Azam <azamdaud2@gmail.com>
2025-12-22 22:48:05 +0500

    fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    VEHICLE_CRM_API_TOKEN: process.env.VEHICLE_CRM_API_TOKEN,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL,
+    NEXT_PUBLIC_VEHICLE_CRM_API_TOKEN: process.env.NEXT_PUBLIC_VEHICLE_CRM_API_TOKEN || process.env.VEHICLE_CRM_API_TOKEN,
+    NEXT_PUBLIC_UNSPLASH_ACCESS_KEY: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY,
+  },
   output: 'export',
   trailingSlash: true,
   images: {
